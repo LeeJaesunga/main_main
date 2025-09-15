6a8cd8a0694215af710d5af06af66838a138a9a6
LeeJaesunga <aap10rk@naver.com>
2025-09-15 15:57:00 +0900

    프로젝트 스크립트 추가

--- main.js
+++ main.js
@@ -65,3 +65,32 @@ if (footerFont) {
 }
 
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector("#main .froject_works ul");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+
+  slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    slider.classList.add("active");
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  });
+  slider.addEventListener("mouseleave", () => {
+    isDown = false;
+    slider.classList.remove("active");
+  });
+  slider.addEventListener("mouseup", () => {
+    isDown = false;
+    slider.classList.remove("active");
+  });
+  slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 1.5; // 드래그 속도 조절
+    slider.scrollLeft = scrollLeft - walk;
+  });
+});
